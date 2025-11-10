d31f327cd856aa4c4884b0756214bdb268b50a43
jjaemun <jjaemuuu@gmai.com>
2025-11-10 15:50:27 +0100

    ops

--- src/ops.rs
+++ src/ops.rs
@@ -1,28 +1,51 @@
-#[!feature(portable_simd)]
+#![feature(portable_simd)]
 
-use crate::stt::TriviallyCopyableSimdType;
 use crate::packets::Packet;
 
-use std::ops::{
-    Add,
-    Sub,
-    Mul,
-    Div
-};
+
+use std::simd::*;
+use std::ops::*;
 
 
-macro_rules! declare_overloads {
+
+impl<T: Copy, const N: usize> Packet<T, N> {
+    pub fn new(v: [T; N]) -> Self {
+        Self { v: Simd::from_array(v) }
+    }
+}
+
+
+impl<T, const N: usize> Deref for Packet<T, N> {
+    type Target = Simd<T, N>;
+    fn deref(&self) -> &Self::Target {
+        &self.v
+    }
+}
+
+
+impl<T, const N: usize> DerefMut for Packet<T, N> {
+    type Target = Simd<T, N>;
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.v
+    }
+}
+
+
+macro_rules! declare_arithmetic_overloads {
     ($trait: ident, $func: ident, $op: tt) => {
-        impl<T, const N: usize> $trait for Packet<T, N> {
-            fn $func(self, rhs: Self) -> Self::Packet<T, N> {
+        impl<T, const N: usize> $trait for Packet<T, N> 
+        where 
+            T: Copy + $trait<Output = T>,  
+        {
+            type Output = Self;
+            fn $func(self, rhs: Self) -> Self::Output {
                 Packet { v: self.v $op rhs.v }
             }
         }
     };
 }
 
-
-declare_overloads!(Add, add, +);
-declare_overloads!(Sub, sub, -);
-declare_overloads!(Mul, mul, *);
-declare_overloads!(Div, div, /);
+declare_arithmetic_overloads!(Add, add, +);
+declare_arithmetic_overloads!(Sub, sub, -);
+declare_arithmetic_overloads!(Mul, mul, *);
+declare_arithmetic_overloads!(Div, div, /);
--- src/stt.rs
+++
@@ -1,8 +0,0 @@
-#!feature[(portable_simd)]
-
-
-use std::simd::{*};
-
-
-pub trait TriviallyCopyableSimdType : Copy + std::simd::SimdElement {}
-impl<T: Copy + std::simd::SimdElement> TriviallyCopyableSimdType for T {}
