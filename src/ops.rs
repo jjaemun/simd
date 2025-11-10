b49e943b2a0d92fe1793f697e845e12b9fe18446
jjaemun <jjaemuuu@gmai.com>
2025-11-10 17:12:25 +0100

    ops

--- src/ops.rs
+++ src/ops.rs
@@ -49,3 +49,81 @@ declare_arithmetic_overloads!(Add, add, +);
 declare_arithmetic_overloads!(Sub, sub, -);
 declare_arithmetic_overloads!(Mul, mul, *);
 declare_arithmetic_overloads!(Div, div, /);
+
+
+macro_rules! declare_sv_arithmetic_overloads {
+    ($trait: ident, $func: ident, $op: tt) => {
+        impl<T, const N: usize> $trait<Packet<T,N>> for T
+        where 
+            T: Copy + $trait<Output = T>,  
+        {
+            type Output = Packet<T, N>;
+            fn $func(self, rhs: Packet<T, N>) -> Self::Output {
+                Packet { v: Simd::splat(self) $op rhs.v }
+            }
+        }
+    };
+}
+
+declare_sv_arithmetic_overloads!(Add, add, +);
+declare_sv_arithmetic_overloads!(Sub, sub, -);
+declare_sv_arithmetic_overloads!(Mul, mul, *);
+declare_sv_arithmetic_overloads!(Div, div, /);
+
+
+macro_rules! declare_vs_arithmetic_overloads {
+    ($trait: ident, $func: ident, $op: tt) => {
+        impl<T, const N: usize> $trait<T> for Packet<T, N>
+        where 
+            T: Copy + $trait<Output = T>,  
+        {
+            type Output = Packet<T, N>;
+            fn $func(self, rhs: T) -> Self::Output {
+                Packet { v: self.v $op Simd::splat(rhs) }
+            }
+        }
+    };
+}
+
+declare_vs_arithmetic_overloads!(Add, add, +);
+declare_vs_arithmetic_overloads!(Sub, sub, -);
+declare_vs_arithmetic_overloads!(Mul, mul, *);
+declare_vs_arithmetic_overloads!(Div, div, /);
+
+
+macro_rules! declare_assign_overloads {
+    ($trait: ident, $func: ident, $op: tt) => {
+        impl <T, const N: usize> $trait for Packet<T, N> 
+        where
+            T: Copy + $trait<Output = T>,
+        {
+            fn $func(&mut self, rhs: Self) {
+                self.v = self.v $op rhs.v;
+            }
+        }
+    };
+}
+
+declare_assign_overloads!(AddAssign, add_assign, +);
+declare_assign_overloads!(SubAssign, sub_assign, -);
+declare_assign_overloads!(MulAssign, mul_assign, *);
+declare_assign_overloads!(DivAssign, div_assign, /);
+
+
+macro_rules! declare_scalar_assign_overloads {
+    ($trait: ident, $func: ident, $op: tt) => {
+        impl <T, const N: usize> $trait<T> for Packet<T, N> 
+        where
+            T: Copy + $trait<Output = T>,
+        {
+            fn $func(&mut self, rhs: T) {
+                self.v = self.v $op Simd::splat(rhs);
+            }
+        }
+    };
+}
+
+declare_scalar_assign_overloads!(AddAssign, add_assign, +);
+declare_scalar_assign_overloads!(SubAssign, sub_assign, -);
+declare_scalar_assign_overloads!(MulAssign, mul_assign, *);
+declare_scalar_assign_overloads!(DivAssign, div_assign, /);
