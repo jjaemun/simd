1759e6097e2888ffdd2d4f8da5e06f8a60851cc8
jjaemun <jjaemuuu@gmail.com>
2025-12-02 22:18:34 +0100

    improvements

--- src/packet/cmp.rs
+++ src/packet/cmp.rs
@@ -25,3 +25,27 @@ where
     LaneCount<N>: SupportedLaneCount,
     Simd<T, N>: Eq,
 {}
+
+impl<T, const N: usize> PartialOrd for Packet<T, N>
+where
+    T: SimdElement + PartialOrd,
+    LaneCount<N>: SupportedLaneCount,
+    Simd<T, N>: PartialOrd,
+{
+    #[inline]
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.v.partial_cmp(&other.v)
+    }
+}
+
+impl<T, const N: usize> Ord for Packet<T, N>
+where
+    T: SimdElement + Ord,
+    LaneCount<N>: SupportedLaneCount,
+    Simd<T, N>: Ord,
+{
+    #[inline]
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.v.cmp(&other.v)
+    }
+}
